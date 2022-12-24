8d0dd9205e6fa77fbb2e02b2ab4248698f4952c2
Angelo Jeffrey Castillo Pérez <107339963+AngeloCastilloPJ@users.noreply.github.com>
2022-12-24 15:04:33 -0500

    Update QR_mod.py

--- QR_mod.py
+++ QR_mod.py
@@ -4,7 +4,7 @@ from PIL import Image
 qr = qrcode.QRCode(version=1,
                    error_correction=qrcode.constants.ERROR_CORRECT_H,
                    box_size=10, border=4,)
-qr.add_data("https://www.linkedin.com/in/sebastgonzales/")
+qr.add_data("COLOCAR TU WEB")
 qr.make(fit=True)
 img = qr.make_image(fill_color="gold", back_colors="white")
 img.save("elchebas.png")
